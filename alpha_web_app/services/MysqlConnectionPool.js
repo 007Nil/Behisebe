72d4ab019f41520ff70986ea10d2b3c3a2f8e6e4
007Nil <sagniksarkar@ymail.com>
2023-03-08 23:07:10 +0530

    added mysqlConnectionPool

--- alpha_web_app/services/MysqlConnectionPool.js
+++ alpha_web_app/services/MysqlConnectionPool.js
@@ -1,1 +1,11 @@
+const mysql = require('mysql2/promise');
+const mysqlParam =  require("./ReadConfig");
 
+const pool = mysql.createPool({
+    connectionLimit: 100, //important
+    host: mysqlParam.MysqlServer,
+    user: mysqlParam.MysqlUser,
+    password: mysqlParam.MysqlPassword,
+    database: mysqlParam.MysqlDatabase,
+    debug: false
+});
