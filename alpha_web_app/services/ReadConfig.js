a0bb86042223df3644015646ccff13c7af2c1191
007Nil <sagniksarkar@ymail.com>
2023-03-10 04:04:10 +0530

    Use abs path for config file

--- alpha_web_app/services/ReadConfig.js
+++ alpha_web_app/services/ReadConfig.js
@@ -1,13 +1,16 @@
 const yaml = require('js-yaml');
 const fs   = require('fs');
+const path = require("path");
 
 function readConfig(){
+    let absoluteConfigPath = path.resolve("alpha_web_app","../configaration/config.yml");
     try{
-        const masterConfig = yaml.load(fs.readFileSync("../configaration/config.yml","utf8"));
+        const masterConfig = yaml.load(fs.readFileSync(absoluteConfigPath,"utf8"));
+        // console.log(masterConfig)
         return masterConfig;
     } catch (error){
-        console.log(e);
+        console.log(error);
     }
 }
 
-module.exports = readConfig;
+module.exports = { readConfig };
