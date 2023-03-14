39dc4f987ff7805c103452f730908e72088db6bc
007Nil <sagniksarkar@ymail.com>
2023-03-15 02:46:43 +0530

    added favicon icon

--- alpha_web_app/index.js
+++ alpha_web_app/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var favicon = require('serve-favicon');
 const cookieParser = require("cookie-parser");
 const expressSession = require('express-session');
 
@@ -14,6 +15,7 @@ const oneDay = 1000 * 60 * 60 * 24;
 var app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
