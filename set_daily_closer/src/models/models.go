2a6167a20ee17fc2a9439f21574c542536ba296d
007Nil <sagniksarkar@ymail.com>
2023-04-10 01:57:47 +0530

    Utility ready for update daily closing amount

--- set_daily_closer/src/models/models.go
+++ set_daily_closer/src/models/models.go
@@ -1,10 +1,25 @@
 package models
 
-type Bank struct {
-	Id string `json:"BankID"`
+type User struct {
+	Id string `json:"ID"`
 }
 
-type User struct {
-	Id          string `json:"ID"`
-	BankDetails Bank
+type Bank struct {
+	Id     string `json:"BankID"`
+	UserId string `json:"UserID"`
+}
+
+type DailyClosing struct {
+	Id     string `json:"ID"`
+	UserId string `json:"UserID"`
+	BankId string `json:"BankID"`
+	Amount int    `json:"Amount"`
+	Date   string `json:"Date"`
+}
+
+type DailyClosingCash struct {
+	Id     string `json:"ID"`
+	UserId string `json:"UserID"`
+	Amount int    `json:"Amount"`
+	Date   string `json:"Date"`
 }
