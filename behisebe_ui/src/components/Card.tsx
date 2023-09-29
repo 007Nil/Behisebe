958d5b05de6e0d40393a5a212cfeadc3483e9a56
007Nil <sagniksarkar@ymail.com>
2023-09-30 02:28:37 +0530

    UI devlopment in progress

--- behisebe_ui/src/components/Card.tsx
+++ behisebe_ui/src/components/Card.tsx
@@ -1,7 +1,7 @@
 export const Card = () => {
     return (
         <>
-            <div id="card" className="relative w-96 h-60 rounded-2xl font-mono text-white overflow-hidden cursor-pointer transition-all duration-500">
+            <div className="relative w-30  lg:w-96 md:w-50 h-60 rounded-2xl font-mono text-white overflow-hidden cursor-pointer transition-all duration-500">
 
 
                 <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center gap-6 p-6 bg-gradient-to-tr from-gray-900 to-gray-700 transition-all duration-100 delay-200 z-20" style={{ transform: "rotateY(0deg)" }}>
@@ -36,28 +36,6 @@ export const Card = () => {
 
                 </div>
 
-                <div className="absolute top-0 left-0 w-full h-full flex flex-col gap-3 justify-center bg-gradient-to-tr from-gray-900 to-gray-700 transition-all z-10"
-                    style={{ transform: "rotateY(180deg)" }}>
-
-
-                    <div className="w-full h-12 bg-black"></div>
-
-                    <div className="px-6 flex flex-col gap-6 justify-center">
-                        <div className="flex flex-col items-end">
-                            <label>CVV</label>
-                            <input type="text" id="" value="123" className="outline-none rounded text-black w-full h-8 text-right"
-                                style={{ background: "repeating-linear-gradient(45deg, #ededed, #ededed 5px, #f9f9f9 5px, #f9f9f9 10px)" }} />
-                        </div>
-
-
-                        <div className="flex justify-start items-center">
-                            <img src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png"
-                                alt="" className="w-12" />
-                        </div>
-
-                    </div>
-
-                </div>
             </div>
         </>
     );
--- behisebe_ui/src/pages/Dashboard.tsx
+++ behisebe_ui/src/pages/Dashboard.tsx
@@ -3,24 +3,42 @@ import { Card } from "../components"
 export const Dashboard = () => {
   return (
     <>
-      <div className="grid grid-cols-2">
-        <div className="col-span-1">
-          <div className="grid grid-cols-2">
-            <div className="col-span-2">
-            </div>
-            <div className="col-span-1">
-              Part 2
-            </div>
-            <div className="col-span-1">
-              part 3
-            </div>
+      <div className="mx-auto">
+        <div className="grid sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
+          <div className="lg:col-span-2 sm:col-span-1">
+            1
           </div>
-        </div>
-        <div className="">
 
-        <Card/>
+          <div className="container">
+            <div className="flex flex-row">
+              <div className="grow">
+                My Funds
+              </div>
+
+              <div className="">
+                add funds
+              </div>
+            </div>
+
+            <div>
+              <Card />
+            </div>
+
+          </div>
+
+          <div className="sm:col-span-3">
+            2asdasdadada
+          </div>
+          <div>
+            3
+          </div>
+
+          <div>
+            5
+          </div>
 
         </div>
+
       </div>
     </>
   )
