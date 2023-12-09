4de35dce1daf46a467664f367febdcefb0227db4
007Nil <sagniksarkar@ymail.com>
2023-12-09 23:46:42 +0530

    deleted old codebase

--- behisebe_ui/src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-export { Button } from './Button'
-export { Navbar } from './Navbar'
-export { Sidebar } from './Sidebar'
-export { Card } from './Card'
-export { PositiveAndNegativeBarChart } from "./Charts/PositiveAndNegativeBarChart"
-export { PieChart} from "./Charts/PieChart"
