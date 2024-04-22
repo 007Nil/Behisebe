1c68a635ef7b98f7cbb79677be4f3983c963951e
007Nil <sagniksarkar@ymail.com>
2024-04-23 04:00:44 +0530

    added docker related files

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" = "up" ]; then
+
+  cd set_daily_closer/src
+  go build -o ../bin/daily_closer . 
+  cd ../../
+  cp set_daily_closer/bin/daily_closer web_app
+  docker compose up
+  rm -rf web_app/daily_closer
+  
+elif [ "$1" = "down" ]; then
+
+  docker compose down --rmi all
+  
+fi
+
+
+
+
