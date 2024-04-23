ce0e6a35b9eca43eebdfc972ad32ad2c9c5e8d6f
007Nil <sagniksarkar@ymail.com>
2024-04-23 20:53:20 +0530

    restore sign up functionality

--- web_app/view_controller/LoginRouter.js
+++ web_app/view_controller/LoginRouter.js
@@ -4,16 +4,6 @@ const {findUserByEmail} = require("../services/UserServices")
 // const User = require("../model/UserModel");
 
 router.get("/",async (req, res) => {
-    // This is for testing only
-    // 
-    // session.userEmail = "sagniksarkar@ymail.com";
-    // let userObject = await findUserByEmail("sagniksarkar@ymail.com");
-    // let session = req.session;
-    // session.userData = userObject[0];
-    // console.log(userObject[0]);
-    // let user = new User();
-    // console.log(user);
-    // res.redirect('/dashboard');
     res.render("Login")
 });
 
